128f107c4d10a49c7d29997da16cd3d92c5e438e
Bruno Freijomil <bruno_freijomil@hotmail.com>
2023-09-21 20:25:40 -0300

    ya se reciben los mensajes del chat

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 const express = require('express')
 const app = express()
-const bodyParser = require("body-parser")
 const exphbs = require('express-handlebars')
 
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({extended:true}))
-
+// handlebars
 app.engine("hbs", exphbs.engine({
     defaultLayout: "main",
     extname: "hbs"
 }))
-
 app.set("view engine", "hbs")
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-// WEBSOCKET:
+// WEBSOCKET (socket.io)
 const server = require('http').Server(app) //crea un servidor HTTP que está listo para manejar las solicitudes HTTP que llegan a tu aplicación Express.
-const io = require('socket.io')(server) //crea una instancia de Socket.IO y la adjunta al servidor HTTP y habilita la funcionalidad de WebSocket
+const ioServidor = require('socket.io')(server) //crea una instancia de Socket.IO y la adjunta al servidor HTTP y habilita la funcionalidad de WebSocket
 
-io.on("connection", function (socketCliente) { //"socketCliente" es el cliente conectado.
-    //aca va lo que quiero hacer cuando el cliente se conecte.
-    console.log("nuevo cliente conectado");
-    socketCliente.emit("mensaje", "Bienvenido")
+ioServidor.on('connection', (socketCliente) => { //"socketCliente" es el cliente conectado.
+    //aca va lo que quiero hacer cuando el cliente se conecte o realice una acción.
+    console.log('Nuevo cliente conectado: ' + socketCliente.id)
+    socketCliente.emit("ejemplo-nombre del mensaje", "Aca va el mensaje en sí")
+    socketCliente.emit("chat message", "Bienvenido a la sala de chat")
+
+    //el mensaje que el cliente envíe al servidor lo recibe "ioServidor" osea, se renviará a cada cliente que se conecte a ese server.
+    socketCliente.on('chat message', (mensaje) => {
+        ioServidor.emit('chat message', mensaje)
+    })
 })
 
-app.listen(8080, () => {
+
+
+server.listen(8080, () => {
     console.log("servidior iniciado en 8080")
 })
